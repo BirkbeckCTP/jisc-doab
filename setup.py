453e483d7cb1ff9323fbcb1f9154bcaf171e1227
Mauro MSL <mauro.msl@outlook.com>
2019-06-07 11:33:19 +0100

    Adds initial setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jisc-doab",
+    version="0.0.1",
+    author="Mauro Sanchez",
+    author_email="mauro.sanchez@openlibhums.org",
+    description="A set of tools for the completion of the Open Metrics for "
+                "Monographs Experiment",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mauromsl/jisc-doab",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "requests",
+        "sickle",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        # "License :: OSI Approved :: TBD",
+        "Operating System :: OS Independent",
+    ],
+)
