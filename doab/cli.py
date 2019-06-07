0743a8b8b21da38a3878030d60e2cdd2eaa00c54
Mauro MSL <mauro.msl@outlook.com>
2019-06-07 13:30:24 +0100

    Adds option to extract the entire repository

--- doab/cli.py
+++ doab/cli.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 
 from doab.client import DOABOAIClient
 from doab import const
@@ -19,7 +20,7 @@ extract_parser = subparsers.add_parser(
 extract_parser.add_argument(
     "publisher_id",
     help="The identifier for the publisher in DOAB",
-    type=int
+    type=str
 )
 
 publishers_parser = subparsers.add_parser(
@@ -31,7 +32,10 @@ publishers_parser = subparsers.add_parser(
 def extract_corpus_for_publisher_id(publisher_id):
     writer = FileManager("out")
     client = DOABOAIClient()
-    records = client.fetch_records_for_publisher_id(publisher_id)
+    if publisher_id == "all":
+        records = client.fetch_all_records()
+    else:
+        records = client.fetch_records_for_publisher_id(publisher_id)
     for record in records:
         print(f"Ectracting Corpus for DOAB record with ID {record.doab_id}")
         record.persist(writer)
@@ -48,6 +52,26 @@ COMMANDS_MAP = {
 }
 
 
+def publisher_validator(arg):
+    """ Ensures that the publihser id argument is either an int or 'all' """
+    if arg == "all":
+        response = ""
+        while response.lower() not in {"y", "n"}:
+            response = input(
+                "WARNING! You are requesting to extract the entire DOAB "
+                "database. Proceed? [y/N]: "
+            )
+        if response.lower() == "n":
+            exit()
+
+    elif arg.is_digit():
+        arg = int(arg)
+    else:
+        raise TypeError("'%s' is not a valid publisher id")
+
+    return arg
+
+
 def run():
     args = parser.parse_args()
     if args.incantation not in COMMANDS_MAP:
@@ -55,6 +79,11 @@ def run():
     else:
         command, arg_names = COMMANDS_MAP[args.incantation]
         command(*(getattr(args, arg) for arg in arg_names))
+
+
+def exit():
+    print("Bye!")
+    sys.exit(0)
 
 
 if __name__ == "__main__":
