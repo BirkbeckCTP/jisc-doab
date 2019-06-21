7d14dd5ace2a99fb42cbc135095e1dab526c6473
mauromsl <mauro.msl@outlook.com>
2019-06-21 09:21:56 +0100

    Run extraction of books on separate threads

--- doab/cli.py
+++ doab/cli.py
@@ -1,11 +1,9 @@
 import argparse
 import logging
 import sys
+from timeit import default_timer as timer
 
-from doab.client import DOABOAIClient
-from doab import const
-from doab.files import FileManager
-
+from doab.commands import extractor, print_publishers
 EXTRACT_CMD = "extract"
 PUBLISHERS_CMD = "publishers"
 PUBLISHER_ID = "publisher_id"
@@ -65,26 +63,9 @@ publishers_parser = subparsers.add_parser(
 )
 
 
-def extract_corpus_for_publisher_id(publisher_id, output_path):
-    writer = FileManager(output_path)
-    client = DOABOAIClient()
-    if publisher_id == "all":
-        records = client.fetch_all_records()
-    else:
-        records = client.fetch_records_for_publisher_id(publisher_id)
-    for record in records:
-        print(f"Ectracting Corpus for DOAB record with ID {record.doab_id}")
-        record.persist(writer)
-
-
-def print_publishers():
-    for pub in const.Publisher:
-        print(f"{pub.value}\t{pub.name}")
-
-
 COMMANDS_MAP = {
     EXTRACT_CMD: (
-        extract_corpus_for_publisher_id,
+        extractor,
         (PUBLISHER_ID, OUTPUT_PATH)
     ),
     PUBLISHERS_CMD: (print_publishers, "")
@@ -100,7 +81,11 @@ def run():
         parser.print_help()
     else:
         command, arg_names = COMMANDS_MAP[args.incantation]
+        start = timer()
         command(*(getattr(args, arg) for arg in arg_names))
+        end = timer()
+        if args.debug:
+            print(end - start)
 
 
 def exit():
---
+++ doab/commands.py
@@ -0,0 +1,27 @@
+from concurrent.futures import ThreadPoolExecutor
+
+from doab import const
+from doab.client import DOABOAIClient
+from doab.files import FileManager
+
+
+def print_publishers():
+    for pub in const.Publisher:
+        print(f"{pub.value}\t{pub.name}")
+
+
+def extractor(publisher_id, output_path):
+    executor = ThreadPoolExecutor(max_workers=15)
+    writer = FileManager(output_path)
+    client = DOABOAIClient()
+    if publisher_id == "all":
+        records = client.fetch_all_records()
+    else:
+        records = client.fetch_records_for_publisher_id(publisher_id)
+    for record in records:
+        print(f"Ectracting Corpus for DOAB record with ID {record.doab_id}")
+        executor.submit(record.persist, writer)
+        writer.write_bytes(
+            filename="metadata.json",
+            to_write=record.export_metadata().encode("utf-8"),
+        )
