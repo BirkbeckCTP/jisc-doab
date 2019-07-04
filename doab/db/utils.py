27deb29f23b1d7dee168d631361755ba271e058e
mauromsl <mauro.msl@outlook.com>
2019-07-04 19:57:04 +0100

    Adds command to populate the db to the CLI

--- doab/db/utils.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def get_dsn():
-    return "postgres://%s:%s@%s/%s" % (
-        os.getenv("DOAB_DB_USER", "root"),
-        os.getenv("DOAB_DB_PASSWORD", "root"),
-        os.getenv("DOAB_DB_HOST", "db"),
-        os.getenv("DOAB_DB_NAME", "doab"),
-    )
