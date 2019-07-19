3ac52c93e1716700ca1d8cf678fcc7e5892c0173
Martin Paul Eve <martin@martineve.com>
2019-07-19 10:19:35 +0100

    Update Palgrave test

--- doab/tests/acceptance_tests.py
+++ doab/tests/acceptance_tests.py
@@ -4,4 +4,4 @@ from doab.tests.test_types import AcceptanceTest, TestManager
 @TestManager.register
 class PalgraveAcceptanceTest(AcceptanceTest):
     CITATION = "Foucault,  M.  (1991).  Discipline  and  Punish.  The  Birth  of  the  Prison  St.  Ives:  Penguin"
-    BOOK_IDS = {"24596", "24598"}
+    BOOK_IDS = {"24596", "20716", "27401"}
