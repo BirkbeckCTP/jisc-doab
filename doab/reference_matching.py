e1d23400090d29a4fc2818a794677bcbb69a0c60
Martin Paul Eve <martin@martineve.com>
2019-07-25 11:58:15 +0100

    Add handling of Cermine/Bibtext fails

--- doab/reference_matching.py
+++ doab/reference_matching.py
@@ -16,7 +16,7 @@ def match(reference):
 def match_by_doi(reference):
     matches = []
 
-    if "doi" not in reference or reference["doi"] is None:
+    if not reference or "doi" not in reference or reference["doi"] is None:
         return matches
 
     with session_context() as session:
@@ -29,7 +29,7 @@ def match_by_doi(reference):
 
 
 def match_title_exact(reference):
-    if "title" not in reference or reference["title"] is None:
+    if not reference or "title" not in reference or reference["title"] is None:
         return []
     with session_context() as session:
         parses_matching = session.query(
@@ -42,7 +42,7 @@ def match_title_exact(reference):
 
 def match_title_fuzzy(reference):
     #TODO: Add weighting by match score
-    if "title" not in reference or reference["title"] is None:
+    if not reference or "title" not in reference or reference["title"] is None:
         return []
     title = reference["title"]
     match_term = f"%{title}%"
