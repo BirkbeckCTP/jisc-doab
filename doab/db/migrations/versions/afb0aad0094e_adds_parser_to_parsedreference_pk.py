035ad3c2295dc59aecc1145e88d6ed45440a7ede
Mauro MSL <mauro.msl@outlook.com>
2019-07-26 18:07:06 +0100

    Revision to add parse to the PK of ParsedReference

---
+++ doab/db/migrations/versions/afb0aad0094e_adds_parser_to_parsedreference_pk.py
@@ -0,0 +1,24 @@
+"""Adds parser to ParsedReference PK
+
+Revision ID: afb0aad0094e
+Revises: fc899b721c89
+Create Date: 2019-07-26 17:05:51.792311
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'afb0aad0094e'
+down_revision = 'fc899b721c89'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
