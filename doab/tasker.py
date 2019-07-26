8618f4276c9ab329c24585c2f62fa6281ef891d9
Mauro MSL <mauro.msl@outlook.com>
2019-07-26 20:38:20 +0100

    Tasker to collect results

--- doab/tasker.py
+++ doab/tasker.py
@@ -7,12 +7,15 @@ logger = logging.getLogger(__name__)
 def run(task, items, msg, workers, *args, **kwargs):
     total_items = len(items)
     executor = ThreadPoolExecutor(max_workers=workers or 1)
+    results = []
     for item_no, item in enumerate(items):
-        log_msg = f"{msg} {item}\t\t[{item_no}/{total_items}]"
+        log_msg = f"[task:{item_no}/{total_items}] {msg} {item}"
         if workers:
             executor.submit(task_wrapper, task, log_msg, item, *args, **kwargs)
         else:
-            task_wrapper(task, log_msg, item, *args, **kwargs)
+            result = task_wrapper(task, log_msg, item, *args, **kwargs)
+            results.append(result)
+    return results
 
 
 def task_wrapper(task, msg, *args, **kwargs):
