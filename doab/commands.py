6f3adb784addf92bb972d54a1aa4be54ffc07f45
Martin Paul Eve <martin@martineve.com>
2019-07-29 19:28:13 +0100

    Nuke command nukes all if no args provided

--- doab/commands.py
+++ doab/commands.py
@@ -155,22 +155,37 @@ def upsert_identifier(session, identifier_str):
 
 def nuke_citations(book_ids=None):
     with session_context() as session:
-        for book_id in book_ids:
-            # fetch book metadata
-            try:
-                book = session.query(
-                    models.Book
-                ).filter(models.Book.doab_id == str(book_id)).one()
+        if not book_ids:
+            books = session.query(models.Book).all()
+            for book in books:
+                try:
+                    for reference in book.references:
+                        for parsed_ref in reference.parsed_references:
+                            session.delete(parsed_ref)
+                        session.delete(reference)
 
-                for reference in book.references:
-                    for parsed_ref in reference.parsed_references:
-                        session.delete(parsed_ref)
-                    session.delete(reference)
+                    session.commit()
+                    print(f'Nuked references for book {book.doab_id}')
+                except NoResultFound:
+                    logger.error(f'Error retrieving book {book.doab_id}.')
+        else:
 
-                session.commit()
-                print(f'Nuked references for book {book.doab_id}')
-            except NoResultFound:
-                logger.error(f'Error retrieving book {book_id}.')
+            for book_id in book_ids:
+                # fetch book metadata
+                try:
+                    book = session.query(
+                        models.Book
+                    ).filter(models.Book.doab_id == str(book_id)).one()
+
+                    for reference in book.references:
+                        for parsed_ref in reference.parsed_references:
+                            session.delete(parsed_ref)
+                        session.delete(reference)
+
+                    session.commit()
+                    print(f'Nuked references for book {book.doab_id}')
+                except NoResultFound:
+                    logger.error(f'Error retrieving book {book_id}.')
 
 
 def print_citations(book_ids=None):
